21a7b86cba8daf76601b82d16482a6bbadb64751
alanmdsan <alanmdsan@gmail.com>
2023-11-27 22:01:00 -0300

    separating favorties from others

--- src/containers/Notes/index.tsx
+++ src/containers/Notes/index.tsx
@@ -1,3 +1,4 @@
+import { nanoid } from 'nanoid'
 import * as S from './styles'
 import Note from '../../components/Note'
 import NoteModel from '../../models/Note'
@@ -14,54 +15,127 @@ const Notes = ({ search }: Props) => {
   const initialState: NotesState = {
     itens: [
       {
-        id: 'hash1',
+        id: nanoid(),
         title: 'estudar JS',
         description:
           'estudar próximos capítulos de js Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
-        favorite: false,
-        color: '#fff'
+        favorite: true,
+        color: '#ffffff'
       },
       {
-        id: 'hash2',
+        id: nanoid(),
         title: 'estudar TS',
         description: 'estudar próximos capítulos de ts',
-        favorite: true,
+        favorite: false,
         color: '#bae2ff'
       },
       {
-        id: 'hash3',
+        id: nanoid(),
         title: 'estudar React pt1',
         description: 'estudar próximos capítulos de react 1',
-        favorite: false,
+        favorite: true,
         color: '#daff8b'
       },
       {
-        id: 'hash4',
+        id: nanoid(),
         title: 'estudar React pt2',
         description: 'estudar próximos capítulos de react 2',
+        favorite: true,
+        color: '#f99494'
+      },
+      {
+        id: nanoid(),
+        title: 'estudar React pt3',
+        description: 'estudar próximos capítulos de react 3',
         favorite: true,
         color: '#f99494'
       }
     ]
   }
 
+  function numberOfFavorites() {
+    return initialState.itens
+      .filter((item) => item.favorite)
+      .filter((item) => item.title.toLowerCase().includes(search.toLowerCase()))
+      .length
+  }
+
+  function numberOfNotFavorites() {
+    return initialState.itens
+      .filter((item) => !item.favorite)
+      .filter((item) => item.title.toLowerCase().includes(search.toLowerCase()))
+      .length
+  }
+
   return (
     <S.ExternalContainer>
       <S.Container>
-        {initialState.itens
-          .filter((item) =>
-            item.title.toLowerCase().includes(search.toLowerCase())
-          )
-          .map((n) => (
-            <Note
-              key={n.id}
-              id={n.id}
-              title={n.title}
-              description={n.description}
-              favorite={n.favorite}
-              color={n.color}
-            />
-          ))}
+        {numberOfFavorites() > 0
+          ? [
+              <div key={nanoid()}>Favoritas</div>,
+              <div key={nanoid()}></div>,
+              <div key={nanoid()}></div>,
+              initialState.itens
+                .filter((item) => item.favorite)
+                .filter((item) =>
+                  item.title.toLowerCase().includes(search.toLowerCase())
+                )
+                .map((n) => (
+                  <Note
+                    key={n.id}
+                    id={n.id}
+                    title={n.title}
+                    description={n.description}
+                    favorite={n.favorite}
+                    color={n.color}
+                  />
+                )),
+              numberOfFavorites() % 3 == 1 ? (
+                <>
+                  <div key={nanoid()}></div>
+                  <div key={nanoid()}></div>
+                </>
+              ) : (
+                ''
+              ),
+              numberOfFavorites() % 3 == 2 ? <div key={nanoid()}></div> : '',
+              numberOfNotFavorites() > 0 ? (
+                <div key={nanoid()}>Outras</div>
+              ) : (
+                ''
+              ),
+              <div key={nanoid()}></div>,
+              <div key={nanoid()}></div>,
+              initialState.itens
+                .filter((item) => !item.favorite)
+                .filter((item) =>
+                  item.title.toLowerCase().includes(search.toLowerCase())
+                )
+                .map((n) => (
+                  <Note
+                    key={n.id}
+                    id={n.id}
+                    title={n.title}
+                    description={n.description}
+                    favorite={n.favorite}
+                    color={n.color}
+                  />
+                ))
+            ]
+          : initialState.itens
+              .filter((item) =>
+                item.title.toLowerCase().includes(search.toLowerCase())
+              )
+              .map((n) => (
+                <Note
+                  key={n.id}
+                  id={n.id}
+                  title={n.title}
+                  description={n.description}
+                  favorite={n.favorite}
+                  color={n.color}
+                />
+              ))}
       </S.Container>
     </S.ExternalContainer>
   )
